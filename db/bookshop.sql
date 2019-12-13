4508fbadb532bc8abebb20b0588fe2bc2eee8aed
Evan Smith <smith.m.evan@gmail.com>
2019-12-13 14:02:49 +0000

    sql file setup

---
+++ db/bookshop.sql
@@ -0,0 +1,19 @@
+DROP TABLE books;
+DROP TABLE publishers;
+
+CREATE TABLE publishers
+(
+  id SERIAL primary key,
+  name VARCHAR(255) not null
+);
+
+CREATE TABLE books
+(
+  id SERIAL primary key,
+  name VARCHAR(255) not null,
+  description VARCHAR(255),
+  quantity INT,
+  price FLOAT,
+  cost FLOAT,
+  publisher_id INT
+);
