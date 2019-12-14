6d042112b0cf01eacd5b1e383eb041f420e4c0a3
Evan Smith <smith.m.evan@gmail.com>
2019-12-14 19:01:23 +0000

    rearranged table structure to account for author, creating many-to-many relationshop and a join table.

--- db/bookshop.sql
+++ db/bookshop.sql
@@ -1,5 +1,8 @@
+DROP TABLE authors_books;
+DROP TABLE authors;
 DROP TABLE books;
 DROP TABLE publishers;
+
 
 CREATE TABLE publishers
 (
@@ -14,6 +17,20 @@ CREATE TABLE books
   description VARCHAR(255),
   quantity INT,
   price FLOAT,
-  cost FLOAT,
-  publisher_id INT references publishers(id) ON DELETE CASCADE 
+  cost FLOAT
 );
+
+CREATE TABLE authors
+(
+  id SERIAL primary key,
+  first_name VARCHAR(255) not null,
+  last_name VARCHAR(255) not null,
+  publisher_id INT references publishers(id)
+);
+
+CREATE TABLE authors_books
+(
+  id SERIAL primary key,
+  author_id INT references authors(id),
+  book_id INT references authors(id)
+)
--- models/book.rb
+++ models/book.rb
@@ -3,7 +3,7 @@ require_relative( '../db/sql_runner' )
 class Book
 
   attr_reader :id
-  attr_accessor :name, :description, :quantity, :price, :cost, :publisher_id
+  attr_accessor :name, :description, :quantity, :price, :cost
 
   def initialize( options )
     @id = options['id'].to_i if options['id']
@@ -12,7 +12,6 @@ class Book
     @quantity = options['quantity'].to_i
     @price = options['price'].to_f
     @cost = options['cost'].to_f
-    @publisher_id = options['publisher_id'].to_i
   end
 
   def save()
@@ -21,9 +20,8 @@ class Book
     description,
     quantity,
     price,
-    cost,
-    publisher_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;"
-    values = [@name, @description, @quantity, @price, @cost, @publisher_id]
+    cost) VALUES ($1, $2, $3, $4, $5) RETURNING id;"
+    values = [@name, @description, @quantity, @price, @cost]
     @id = SqlRunner.run(sql, values)[0]["id"].to_i
   end
 
@@ -50,9 +48,8 @@ class Book
     description,
     quantity,
     price,
-    cost,
-    publisher_id) = ($1, $2, $3, $4, $5, $6) WHERE id = $7"
-    values = [@name, @description, @quantity, @price, @cost, @publisher_id, @id]
+    cost) = ($1, $2, $3, $4, $5) WHERE id = $6"
+    values = [@name, @description, @quantity, @price, @cost, @id]
     SqlRunner.run(sql, values)
   end
 
