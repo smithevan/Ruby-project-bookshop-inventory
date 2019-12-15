4ed0189323449f50fa28490546f3c28d803617f9
Evan Smith <smith.m.evan@gmail.com>
2019-12-15 17:09:46 +0000

    publishers page working

--- controllers/publishers_controller.rb
+++ controllers/publishers_controller.rb
@@ -2,4 +2,11 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry' )
 require_relative( '../models/publisher.rb' )
+require_relative( '../models/author.rb' )
+require_relative( '../models/book.rb' )
 also_reload( '../models/*' )
+
+get '/publishers' do
+  @publishers = Publisher.all()
+  erb ( :"publishers/index" )
+end
