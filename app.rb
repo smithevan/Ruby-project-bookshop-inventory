c47a445d4283ebdeee9f82695b2398896f301c10
Evan Smith <smith.m.evan@gmail.com>
2019-12-15 16:13:11 +0000

    front end shell basic but working

---
+++ app.rb
@@ -0,0 +1,9 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative('controllers/authors_controller')
+require_relative('controllers/books_controller')
+require_relative('controllers/publishers_controller')
+
+get '/' do
+  erb( :index )
+end
