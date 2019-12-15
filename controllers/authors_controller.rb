0c1dd8530d4adcc8070b9bd36b0358fd1b16e56a
Evan Smith <smith.m.evan@gmail.com>
2019-12-15 17:35:59 +0000

    author page working

--- controllers/authors_controller.rb
+++ controllers/authors_controller.rb
@@ -2,4 +2,11 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry' )
 require_relative( '../models/author.rb' )
+require_relative( '../models/publisher.rb' )
+require_relative( '../models/book.rb' )
 also_reload( '../models/*' )
+
+get '/authors' do
+  @authors = Author.all()
+  erb ( :"authors/index" )
+end
