6e9df348f4370a14ad886a045185c9eff30e0c91
Evan Smith <smith.m.evan@gmail.com>
2019-12-15 16:02:03 +0000

    setting up file structure for front end

---
+++ controllers/authors_controller.rb
@@ -0,0 +1,5 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry' )
+require_relative( '../models/author.rb' )
+also_reload( '../models/*' )
---
+++ controllers/books_controller.rb
@@ -0,0 +1,7 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry' )
+require_relative( '../models/book.rb' )
+require_relative( '../models/publisher.rb' )
+require_relative( '../models/author.rb' )
+also_reload( '../models/*' )
