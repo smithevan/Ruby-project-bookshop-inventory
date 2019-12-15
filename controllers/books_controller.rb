7c7c0722e50d60ae11283f8164dd5e453109d8cd
Evan Smith <smith.m.evan@gmail.com>
2019-12-15 16:19:57 +0000

    addint index erb files to the views

--- controllers/books_controller.rb
+++ controllers/books_controller.rb
@@ -5,3 +5,8 @@ require_relative( '../models/book.rb' )
 require_relative( '../models/publisher.rb' )
 require_relative( '../models/author.rb' )
 also_reload( '../models/*' )
+
+get '/books' do
+  @books = Book.all()
+  erb ( :"books/index" )
+end
